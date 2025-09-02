87b53c173a5ae667db85f677c39b018d68c2ca50
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-02 14:06:13 +0800

    format and add qdrant client

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { client } from "./utils/client";
 
 export default function App() {
-  const handleClick = async () => {
-    const res = await client.index.$get();
-    const data = await res.text();
-    alert(data);
-  };
-  return (
-    <div>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  );
+	const handleClick = async () => {
+		const res = await client.index.$get();
+		const data = await res.text();
+		alert(data);
+	};
+	return (
+		<div>
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	);
 }
