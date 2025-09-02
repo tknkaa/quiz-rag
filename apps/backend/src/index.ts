44704385f03502449e308f6bd6285b9d36a4bccd
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-02 14:54:06 +0800

    TLS error

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,6 +3,8 @@ import { cors } from "hono/cors";
 import { QdrantClient } from "@qdrant/js-client-rest";
 import { zValidator } from "@hono/zod-validator";
 import z from "zod";
+import { GoogleGenAI } from "@google/genai";
+import { env } from "hono/adapter";
 
 const app = new Hono();
 const client = new QdrantClient({
@@ -28,7 +30,35 @@ const route = app
 				prompt: z.string(),
 			}),
 		),
-		(c) => {
+		async (c) => {
+			const { GEMINI_API_KEY } = env<{ GEMINI_API_KEY: string }>(c);
+			const ai = new GoogleGenAI({
+				apiKey: GEMINI_API_KEY,
+			});
+			const aiResponse = await ai.models.embedContent({
+				model: "gemini-embedding-001",
+				contents: c.req.valid("form").prompt,
+			});
+			if (!aiResponse.embeddings) {
+				return c.json({
+					ok: false,
+					message: "Failed to get embedding vector",
+				});
+			}
+			const vector = aiResponse.embeddings[0].values;
+			if (!vector) {
+				return c.json({
+					ok: false,
+					message: "Failed to get embedding vector",
+				});
+			}
+			// this collection name is defined in db/seed.py
+			const vecs = await client.search("utcode_learn", {
+				vector: vector,
+				limit: 3,
+			});
+			console.log(vecs);
+
 			return c.json({
 				ok: true,
 				quiz: "what is the best linux distro?",
