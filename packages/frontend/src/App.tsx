8bfc1b1ae47b42cbaed23f8b65102aa10b5b6cef
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-03 09:33:49 +0800

    apps -> packages

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { client } from "./utils/client";
+
+export default function App() {
+	const handleClick = async () => {
+		const res = await client.index.$get();
+		const data = await res.text();
+		alert(data);
+	};
+	return (
+		<div>
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	);
+}
