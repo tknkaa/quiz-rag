59ee9baa46db46bbf7b03dad29b2f6e37349cb9f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-20 22:51:24 +0800

    fix comments for migration

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
+// bunx drizzle-kit generate
+// 1) To migrate remote Db,
 // bunx drizzle-kit migrate
-// bunx wrangler d1 execute daily_code_learn --local --file=...sql
+// 2) To migrate local db
+// bunx wrangler d1 execute daily_code_learn --local --file=path_to_sql_file
 export default defineConfig({
 	out: "./drizzle",
 	schema: "./src/schema.ts",
